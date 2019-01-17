c79e0785ccccbb515cae2b756b201f1af1a1dafc
Andreev Vladimir <andreyevpostbox@gmail.com>
2019-01-17 17:57:16 +0300

    CORRECT TDO SIMULATION

--- JTAG/TAP/Bist.v
+++ JTAG/TAP/Bist.v
@@ -118,16 +118,17 @@ always @(posedge clk) begin
     if (enable) bc <= pc-1;
 end
 
-reg [7:0] CORRECT_CHECK;
+reg [3:0] CORRECT_CHECK;
 
 always @(posedge clk) begin
-  CORRECT_CHECK [7:4] <= CORRECT_CHECK [3:0]; 
-  CORRECT_CHECK [3:0] = BIST_IN; 
+    if (pc_safe-1 < pc) begin
+        CORRECT_CHECK [3:0] = BIST_IN; 
+     end
 end
 
 
 
 assign BIST_STATUS = RESET_SM & !error ? {bist_check[bc-1][4:1], bist_config[bc][4:1], bist_check[bc][4:1], 4'hF} :
-                                         {bist_check[bc-1][4:1], bist_config[bc][4:1], CORRECT_CHECK[7:4],  4'h5} ;
+                                         {bist_check[bc-1][4:1], bist_config[bc][4:1], CORRECT_CHECK[3:0],  4'h5} ;
 
 endmodule
